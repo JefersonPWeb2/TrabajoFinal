2fb7a50906dbe88b123040893ce622d87c9a5416
Jeferson <jbasurco@unsa.edu.pe>
2024-07-01 09:16:19 -0500

    Definiendo los modelos Categoria y Producto

--- ProyectoFinal/Backend/Tienda/Inventario/models.py
+++ ProyectoFinal/Backend/Tienda/Inventario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=100)
+
+class Producto(models.Model):
+    name = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10)
