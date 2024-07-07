79fc129485ac8882c1499dd6dceb3439b880b5aa
PaulCari <pcaril@unsa.edu.pe>
2024-07-06 22:38:58 -0500

    En app.compnent.ts importamos los componentes navbar y product-card ademas añadimos los productos, nombre del producto la imagen del producto y si es nuevo

--- ProyectoFinal/Fronted/Tienda/src/app/app.component.ts
+++ ProyectoFinal/Fronted/Tienda/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ProductCardComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tienda';
+  title = 'abylu-store';
+  products = [
+    { name: 'Cardigan Rayas', image: 'cardigan-rayas.png', new: true },
+    { name: 'Chaleco', image: 'chaleco.png', new: true },
+    { name: 'Chompa Offshoulder', image: 'chompa-offshoulder.png', new: true },
+    { name: 'Chompa Corazon', image: 'chompa-corazon.png', new: true },
+    { name: 'Falda Tableada Vane', image: 'falda-tableada-vane.png', new: true }
+  ];
 }
