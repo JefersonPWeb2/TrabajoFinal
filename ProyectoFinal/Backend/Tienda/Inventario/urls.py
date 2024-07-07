84f0e5a54aa0397ff3df1ec7f8c776ce4234c876
Jeferson <jbasurco@unsa.edu.pe>
2024-07-06 20:26:22 -0500

    Corrigiendo rutas

--- ProyectoFinal/Backend/Tienda/Inventario/urls.py
+++ ProyectoFinal/Backend/Tienda/Inventario/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.producto_lista, name='producto_list'),
-    path('productos/<int:pk>/', views.producto_detalle, name='producto_detail'),
+    path('productos/', views.producto_lista, name='producto_lista'),
+    path('productos/<int:pk>/', views.producto_detalle, name='producto_detalle'),
     path('productos/nuevo/', views.producto_create, name='producto_create'),
     path('productos/<int:pk>/edit/', views.producto_update, name='producto_update'),
     path('productos/<int:pk>/delete/', views.producto_delete, name='producto_delete'),
