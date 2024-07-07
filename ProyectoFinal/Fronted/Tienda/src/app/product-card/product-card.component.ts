e893c1a356746914924184e383e3da9fe8e13913
PaulCari <pcaril@unsa.edu.pe>
2024-07-06 22:33:14 -0500

    agregamos a product-card.component.ts el input

--- ProyectoFinal/Fronted/Tienda/src/app/product-card/product-card.component.ts
+++ ProyectoFinal/Fronted/Tienda/src/app/product-card/product-card.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
-  standalone: true,
-  imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrls: ['./product-card.component.css'],
+  standalone: true
 })
 export class ProductCardComponent {
-
+  @Input() product: any;
 }
