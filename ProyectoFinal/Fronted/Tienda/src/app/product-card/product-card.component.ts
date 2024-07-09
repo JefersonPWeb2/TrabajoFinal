d32af3fa5be2755f92537ef5505e9cd626d659a0
PaulCari <pcaril@unsa.edu.pe>
2024-07-09 09:21:26 -0500

    modificamos los enlaces de las imagenes en navbar.html

--- ProyectoFinal/Fronted/Tienda/src/app/product-card/product-card.component.ts
+++ ProyectoFinal/Fronted/Tienda/src/app/product-card/product-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
-  standalone: true
+  standalone: true, 
+  imports: [NgIf] 
 })
 export class ProductCardComponent {
   @Input() product: any;
