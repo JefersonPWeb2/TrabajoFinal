b1a735b4c848e6c70486bd792c990399d31839b9
Jeferson <jbasurco@unsa.edu.pe>
2024-07-15 06:27:53 -0500

    Configurando aplicación en settings

--- ProyectoFinal/Backend/Tienda/Tienda/settings.py
+++ ProyectoFinal/Backend/Tienda/Tienda/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'Inventario',
+    'cuentas',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
