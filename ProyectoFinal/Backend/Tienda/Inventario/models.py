fac460050d22c877119a74818c230962501e67e3
Jeferson <jbasurco@unsa.edu.pe>
2024-07-15 06:26:01 -0500

    Agregando metodo devolviendo nombre

--- ProyectoFinal/Backend/Tienda/Inventario/models.py
+++ ProyectoFinal/Backend/Tienda/Inventario/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+    
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
@@ -10,3 +13,5 @@ class Producto(models.Model):
     cantidad = models.IntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to='producto_images/', blank=True, null=True)
+    def __str__(self):
+        return self.nombre
