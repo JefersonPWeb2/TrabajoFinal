63de6548fb3534f8b9cf4fd4a1716cb068c55606
Jeferson <jbasurco@unsa.edu.pe>
2024-07-15 06:30:10 -0500

    Nuevas vistas para el login, register y logout

--- ProyectoFinal/Backend/Tienda/cuentas/views.py
+++ ProyectoFinal/Backend/Tienda/cuentas/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistroForm, LoginForm
 
-# Create your views here.
+def landing_page_view(request):
+    return render(request, 'cuentas/landing_page.html')
+
+def registro_view(request):
+    if request.method == "POST":
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistroForm()
+    return render(request, 'cuentas/registro.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'cuentas/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def home_view(request):
+    return render(request, 'cuentas/home.html')
