c76243839283199d74e11525f3307f2401191053
Jeferson <jbasurco@unsa.edu.pe>
2024-07-15 06:26:26 -0500

    Creando rutas para categoria

--- ProyectoFinal/Backend/Tienda/Inventario/urls.py
+++ ProyectoFinal/Backend/Tienda/Inventario/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('productos/nuevo/', views.producto_create, name='producto_create'),
     path('productos/<int:pk>/edit/', views.producto_update, name='producto_update'),
     path('productos/<int:pk>/delete/', views.producto_delete, name='producto_delete'),
-    path('productos/json/', views.producto_list_json, name='producto_list_json')
+    path('productos/json/', views.producto_list_json, name='producto_list_json'),
+    path('categoria/', views.categoria_lista, name='categoria_lista'),
+    path('categoria/nuevo/', views.categoria_nueva, name='categoria_nueva'),
 ]
