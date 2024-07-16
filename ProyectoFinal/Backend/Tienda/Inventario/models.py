74ac2aaa1c1456906fa464a0cb635d6dde7b287f
PaulCari <pcaril@unsa.edu.pe>
2024-07-16 12:46:57 -0500

    Quitando el atributo cantidad

--- ProyectoFinal/Backend/Tienda/Inventario/models.py
+++ ProyectoFinal/Backend/Tienda/Inventario/models.py
@@ -10,7 +10,6 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to='producto_images/', blank=True, null=True)
     def __str__(self):
