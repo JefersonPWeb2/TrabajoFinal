b5defab64e72161ae119c3f70641b34b4c2efc6a
Jeferson <jbasurco@unsa.edu.pe>
2024-07-21 10:45:49 -0500

    Registrando modelos del carrito al panel de administrador

--- ProyectoFinal/Backend/Tienda/Inventario/admin.py
+++ ProyectoFinal/Backend/Tienda/Inventario/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria, Producto
+from .models import Categoria, Producto, Carrito, CarritoItem
 
 admin.site.register(Categoria)
 admin.site.register(Producto)
+admin.site.register(CarritoItem)
+admin.site.register(Carrito)
