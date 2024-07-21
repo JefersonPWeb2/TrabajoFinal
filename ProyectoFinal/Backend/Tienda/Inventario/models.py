bd8d923490bb1fbebbf4788f5187f90f1a01ff58
Jeferson <jbasurco@unsa.edu.pe>
2024-07-21 10:20:02 -0500

    Definiendo modelos para el carrito de compras

--- ProyectoFinal/Backend/Tienda/Inventario/models.py
+++ ProyectoFinal/Backend/Tienda/Inventario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
@@ -14,3 +15,17 @@ class Producto(models.Model):
     imagen = models.ImageField(upload_to='producto_images/', blank=True, null=True)
     def __str__(self):
         return self.nombre
+
+class Carrito(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Carrito de {self.usuario.username}"
+
+class CarritoItem(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre}"
