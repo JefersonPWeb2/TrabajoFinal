b476fb1c137dab5ed67d7e824514e5c066e032c2
Jeferson <jbasurco@unsa.edu.pe>
2024-07-21 19:53:29 -0500

    Modificando formulario de Inventario

--- ProyectoFinal/Backend/Tienda/Inventario/forms.py
+++ ProyectoFinal/Backend/Tienda/Inventario/forms.py
@@ -4,12 +4,12 @@ from .models import Producto, Categoria, CarritoItem
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ['nombre', 'descripcion', 'precio', 'categoria', 'imagen']
+        fields = ['nombre', 'descripcion', 'categoria', 'precio', 'imagen']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
-            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
             'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
             'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
 class CategoriaForm(forms.ModelForm):
