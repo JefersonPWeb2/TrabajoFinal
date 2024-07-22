3dabe8ceb396523898c7312963532fb9f10e9ba6
Jeferson <jbasurco@unsa.edu.pe>
2024-07-21 20:04:18 -0500

    Nuevas urls para archivos multimedia

--- ProyectoFinal/Backend/Tienda/Tienda/urls.py
+++ ProyectoFinal/Backend/Tienda/Tienda/urls.py
@@ -25,7 +25,4 @@ urlpatterns = [
     path('cuentas/', include('cuentas.urls')),
     path('', landing_page_view, name='landing_page'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
