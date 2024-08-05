1a61c791e1208692e33cee7c3ca303939367457b
Jeferson <jbasurco@unsa.edu.pe>
2024-08-05 12:41:30 -0500

    Agregando componente ProductoForm

--- ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
+++ ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoFormComponent } from './Components/producto-form/producto-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoFormComponent
   ],
   imports: [
     BrowserModule,
