2de3d8dd0fdf7f8ffc2aeae5106bb45ec1d6834f
Jeferson <jbasurco@unsa.edu.pe>
2024-08-05 11:15:52 -0500

    Creando nuevas rutas API's

--- ProyectoFinal/Backend/Tienda/Inventario/urls.py
+++ ProyectoFinal/Backend/Tienda/Inventario/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import ProductoViewSet, CategoriaViewSet, crear_producto, actualizar_producto
 
+router = DefaultRouter()
+router.register(r'productos', ProductoViewSet, basename='producto')
+router.register(r'categorias', CategoriaViewSet, basename='categoria')
 urlpatterns = [
     path('productos/', views.producto_lista, name='producto_lista'),
     path('productos/<int:pk>/', views.producto_detalle, name='producto_detalle'),
     path('productos/nuevo/', views.producto_create, name='producto_create'),
     path('productos/<int:pk>/edit/', views.producto_update, name='producto_update'),
     path('productos/<int:pk>/delete/', views.producto_delete, name='producto_delete'),
-    path('api/productos/', views.producto_list_json, name='producto_list_json'),
-    path('api/categorias/', views.categoria_list_json, name='categoria_list_json'),
+    path('api/', include(router.urls)),
+    path('api/producto/', crear_producto, name='crear_producto'),
+    path('api/productos/<int:pk>/', actualizar_producto, name='actualizar_producto'),
     path('categoria/', views.categoria_lista, name='categoria_lista'),
     path('categoria/nuevo/', views.categoria_nueva, name='categoria_nueva'),
     path('categoria/<int:pk>/delete/', views.categoria_delete, name='categoria_delete'),
