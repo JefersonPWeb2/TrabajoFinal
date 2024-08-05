601e7394848417492ac0db7a93d43ef315308a66
Jeferson <jbasurco@unsa.edu.pe>
2024-08-05 11:30:03 -0500

    Creando servicios para 'Categoria' y 'Producto' para los formularios

---
+++ ProyectoFinal/Fronted/Tienda/src/app/Services/categoria.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Categoria } from '../models/categoria.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+  private apiUrl = 'http://localhost:8000/inventario/api/categorias/';
+
+  constructor(private http: HttpClient) {}
+
+  getCategorias(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(this.apiUrl);
+  }
+}
