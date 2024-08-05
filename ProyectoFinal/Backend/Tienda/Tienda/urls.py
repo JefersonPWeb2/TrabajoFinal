f08453e84d3ecffa1fa39bd0e5860266b21e14d9
Jeferson <jbasurco@unsa.edu.pe>
2024-08-05 11:18:57 -0500

    Agregando rutas para archivos estaticos

--- ProyectoFinal/Backend/Tienda/Tienda/urls.py
+++ ProyectoFinal/Backend/Tienda/Tienda/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('cuentas/', include('cuentas.urls')),
     path('', landing_page_view, name='landing_page'),
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
