435f28e5c2694e24b9e7a3cac287203732210f18
Jeferson <jbasurco@unsa.edu.pe>
2024-08-05 12:49:48 -0500

    Agregando componente ProductoList

--- ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
+++ ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoFormComponent } from './Components/producto-form/producto-form.component';
+import { ProductoListComponent } from './Components/producto-list/producto-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoFormComponent
+    ProductoFormComponent,
+    ProductoListComponent
   ],
   imports: [
     BrowserModule,
