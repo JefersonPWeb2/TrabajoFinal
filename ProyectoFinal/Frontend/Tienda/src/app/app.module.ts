a109630cb5408601f1aa69d3be1ed8d207a93291
Jeferson <jbasurco@unsa.edu.pe>
2024-08-09 16:57:10 -0500

    Importando formularios y servicios Http

--- ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
+++ ProyectoFinal/Frontend/Tienda/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoFormComponent } from './Components/producto-form/producto-form.component';
 import { ProductoListComponent } from './Components/producto-list/producto-list.component';
+import { ProductoService } from './Services/producto.service';
+import { CategoriaService } from './Services/categoria.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductoFormComponent,
-    ProductoListComponent
+    ProductoListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
-    provideClientHydration()
+    ProductoService,
+    CategoriaService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
