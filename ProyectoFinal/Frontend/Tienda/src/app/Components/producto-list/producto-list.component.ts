ee22b875c8162df609e009248661d3e7139890a6
Jeferson <jbasurco@unsa.edu.pe>
2024-08-09 17:01:19 -0500

    Realizando importaciones, creando constructor y creando funciones para la actualización y eliminación de productos

--- ProyectoFinal/Frontend/Tienda/src/app/Components/producto-list/producto-list.component.ts
+++ ProyectoFinal/Frontend/Tienda/src/app/Components/producto-list/producto-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../../Services/producto.service';
+import { Producto } from '../../Models/producto.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-list',
   templateUrl: './producto-list.component.html',
-  styleUrl: './producto-list.component.css'
+  styleUrls: ['./producto-list.component.css']
 })
-export class ProductoListComponent {
+export class ProductoListComponent implements OnInit {
+  productos: Producto[] = [];
 
+  constructor(
+    private productoService: ProductoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.productoService.getProductos().subscribe(data => {
+      this.productos = data;
+    });
+  }
+
+  editarProducto(id: number): void {
+    this.router.navigate(['/productos', id, 'editar']);
+  }
+
+  eliminarProducto(id: number): void {
+    this.productoService.deleteProducto(id).subscribe(() => {
+      this.productos = this.productos.filter(producto => producto.id !== id);
+    });
+  }
 }
