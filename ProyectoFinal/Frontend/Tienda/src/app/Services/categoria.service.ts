fb575e67bf7d899120d020e59338675731633bbe
Jeferson <jbasurco@unsa.edu.pe>
2024-08-09 16:52:45 -0500

    Creando apiURL para el servicio 'Categoría'

--- ProyectoFinal/Frontend/Tienda/src/app/Services/categoria.service.ts
+++ ProyectoFinal/Frontend/Tienda/src/app/Services/categoria.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Categoria } from '../Models/categoria.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
+  private apiUrl = 'http://localhost:8000/inventario/api/categorias/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCategorias(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(this.apiUrl);
+  }
 }
