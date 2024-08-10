efaf3c3d977950f12ce7366bf5e06fcb0375c005
Jeferson <jbasurco@unsa.edu.pe>
2024-08-09 21:50:37 -0500

    Creando componente para una barra lateral para las categorías

---
+++ ProyectoFinal/Frontend/Tienda/src/app/Components/sidebar-categorias/sidebar-categorias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-categorias',
+  templateUrl: './sidebar-categorias.component.html',
+  styleUrl: './sidebar-categorias.component.css'
+})
+export class SidebarCategoriasComponent {
+
+}
